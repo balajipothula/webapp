8faab399f3aae33f1976b152aa4f4dab06e35bda
Balaji Pothula <balan.pothula@gmail.com>
2022-01-17 19:49:03 +0100

    test 2022.01.17.19.48

--- main.tf
+++ main.tf
@@ -81,7 +81,8 @@ resource "aws_iam_role_policy_attachment" "webapp_lambda_policy_attachment" {
     aws_iam_policy.webapp_lambda_policy
   ]
 
-  role       = "WebAppLambdaRole"
+//role       = "WebAppLambdaRole"
+  role       = module.aws_iam_role_webapp.name
   policy_arn = aws_iam_policy.webapp_lambda_policy.arn
 }
 
