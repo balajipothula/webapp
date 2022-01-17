bab4af7f8c844bdbaa698e72818f46d24fd23d27
Balaji Pothula <balan.pothula@gmail.com>
2022-01-17 15:58:28 +0100

    Modified role policy JSON path.

--- terraform/aws/iam/role/main.tf
+++ terraform/aws/iam/role/main.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "generic" {
 
-  assume_role_policy    = file("./terraform/aws/role/json/WebAppLambdaRole.json")
+  assume_role_policy    = file("./terraform/aws/iam/role/json/WebAppLambdaRole.json")
   description           = "Role policy for WebApp Lambda Function."
   force_detach_policies = false 
   name                  = "WebAppLambdaRole"
