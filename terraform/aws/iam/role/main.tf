ff38e97e6c7f5047849c97ad4e5cd3563f6cfd1b
Balaji Pothula <balan.pothula@gmail.com>
2022-01-17 16:17:24 +0100

    testing code.

--- terraform/aws/iam/role/main.tf
+++ terraform/aws/iam/role/main.tf
@@ -4,6 +4,6 @@ resource "aws_iam_role" "generic" {
   assume_role_policy    = file("${path.module}/json/WebAppLambdaRole.json")
   description           = "Role policy for WebApp Lambda Function."
   force_detach_policies = false 
-  name                  = "WebAppLambdaRole"
+  name                  = "WebAppLambdaRole3"
 
 }
