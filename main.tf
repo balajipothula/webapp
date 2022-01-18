ae3bf7956f5346b7dc7d8bad3ec76231d1cad3fc
Balaji Pothula <balan.pothula@gmail.com>
2022-01-18 13:15:10 +0100

    test at 2022.01.18.13.15

--- main.tf
+++ main.tf
@@ -44,17 +44,14 @@ data "aws_security_groups" "default" {
   }
 }
 
-resource "aws_cloudwatch_log_group" "webapp_lambda_log_group" {
-  name = "/aws/lambda/${module.webapp_aws_lambda_function.function_name}"
-//name_prefix       = ""
-/*Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653
-  If retention_in_days is 0, the events in the log group are always retained and never expire.
-*/
-  retention_in_days = 14
-  tags = {
-    Environment = "Dev"
-    Application = "WebApp"
-  }
+module "webapp_aws_iam_role" {
+
+  source                = "./terraform/aws/iam/role"
+
+  description           = "AWS IAM Role for WebApp." # Optional argument but keep it.
+  force_detach_policies = true                       # Optional argument but keep it.
+  name                  = "WebAppLambdaRole"         # Optional argument but keep it.
+
 }
 
 # See also the following AWS managed policy: AWSLambdaBasicExecutionRole
@@ -64,6 +61,8 @@ resource "aws_iam_policy" "webapp_lambda_policy" {
   description = "IAM policy for WebApp Lambda Function."
   policy      = file("./WebAppLambdaPolicy.json")
 }
+
+
 
 resource "aws_iam_role_policy_attachment" "webapp_lambda_policy_attachment" {
 
@@ -77,21 +76,12 @@ resource "aws_iam_role_policy_attachment" "webapp_lambda_policy_attachment" {
 
 }
 
-module "webapp_aws_iam_role" {
-
-  source                = "./terraform/aws/iam/role"
-
-  description           = "AWS IAM Role for WebApp." # Optional argument but keep it.
-  force_detach_policies = true                       # Optional argument but keep it.
-  name                  = "WebAppLambdaRole"         # Optional argument but keep it.
-
-}
-
 module "webapp_aws_lambda_function" {
 
   source                         = "./terraform/aws/lambda/function"
 
   depends_on                     = [
+    module.webapp_aws_iam_role,
     aws_iam_role_policy_attachment.webapp_lambda_policy_attachment,
   ]
 
@@ -114,4 +104,17 @@ module "webapp_aws_lambda_function" {
   }
   timeout                        = 30                                                # Optional argument but keep it.
 
+}
+
+resource "aws_cloudwatch_log_group" "webapp_lambda_log_group" {
+  name = "/aws/lambda/${module.webapp_aws_lambda_function.function_name}"
+//name_prefix       = ""
+/*Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653
+  If retention_in_days is 0, the events in the log group are always retained and never expire.
+*/
+  retention_in_days = 14
+  tags = {
+    Environment = "Dev"
+    Application = "WebApp"
+  }
 }
