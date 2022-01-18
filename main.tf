628597dd3061188264602cbad1f18186e597816e
Balaji Pothula <balan.pothula@gmail.com>
2022-01-18 12:08:10 +0100

    test at 2022.01.18.12.08

--- main.tf
+++ main.tf
@@ -96,49 +96,18 @@ module "aws_lambda_function_webapp" {
     aws_cloudwatch_log_group.webapp_lambda_log_group
   ]
 
-  function_name                  = var.function_name                    # Required argument.
-  role                           = var.role                             # Required argument.
-//architectures                  = var.architectures                    # Optional argument.
-//code_signing_config_arn        = var.code_signing_config_arn          # Optional argument.
-/*
-  dead_letter_config {                                                  # Optional argument block.
-    target_arn                   = var.target_arn                       # Required block argument.
-  }
-*/
-  description                    = var.description                      # Optional argument but keep it.
-/*
-  file_system_config {                                                  # Optional argument block.
-    arn                          = var.efs_arn                          # Required block argument.
-    local_mount_path             = var.local_mount_path                 # Required block argument.
-  }
-*/
-//filename                       = var.filename                         # Optional argument, Conflicts with image_uri, s3_bucket, s3_key, and s3_object_version.
-  handler                        = var.handler                          # Optional argument but keep it.
-/*
-  image_config {                                                        # Optional argument block.
-    command                      = var.command                          # Optional block argument.
-    entry_point                  = var.entry_point                      # Optional block argument.
-    working_directory            = var.working_directory                # Optional block argument.
-  }
-*/
-//image_uri                      = var.image_uri                        # Conflicts with filename, s3_bucket, s3_key, and s3_object_version.
-//kms_key_arn                    = var.kms_key_arn                      # Optional argument.
-//layers                         = var.layers                           # Optional argument.
-  memory_size                    = var.memory_size                      # Optional argument but keep it.
-  package_type                   = var.package_type                     # Optional argument but keep it.
-  publish                        = var.publish                          # Optional argument but keep it.
-  reserved_concurrent_executions = var.reserved_concurrent_executions   # Optional argument but keep it.
-  runtime                        = var.runtime                          # Optional argument but keep it.
-  s3_bucket                      = var.s3_bucket                        # Optional argument but keep it.
-  s3_key                         = var.s3_key                           # Optional argument but keep it, Conflicts with filename and image_uri.
-//s3_object_version              = var.s3_object_version                # Optional argument, Conflicts with filename and image_uri.
-//source_code_hash               = var.source_code_hash                 # Optional argument.
-  tags                           = var.tags                             # Optional argument but keep it.
-  timeout                        = var.timeout                          # Optional argument but keep it.
-/*
-  tracing_config {                                                      # Optional argument block.
-    mode                         = var.mode                             # Required block argument.
-  }
-*/
+  function_name                  = "WebAppFastAPI"                                   # Required argument.
+  role                           = "arn:aws:iam::304501768659:role/WebAppLambdaRole" # Required argument.
+  description                    = "Web App FastAPI Lambda Function."                # Optional argument but keep it.
+  handler                        = "lambda_function.lambda_handler"                  # Optional argument but keep it.
+  memory_size                    = 128                                               # Optional argument but keep it.
+  package_type                   = "Zip"                                             # Optional argument but keep it.
+  publish                        = false                                             # Optional argument but keep it.
+  reserved_concurrent_executions = -1                                                # Optional argument but keep it.
+  runtime                        = "python3.8"                                       # Optional argument but keep it.
+  s3_bucket                      = "job-log-s3-bucket"                               # Optional argument but keep it.
+  s3_key                         = "v1.0.3/job-log.zip"                              # Optional argument but keep it, Conflicts with filename and image_uri.
+//tags                           = var.tags                                          # Optional argument but keep it.
+  timeout                        = 30                                                # Optional argument but keep it.
 
 }
