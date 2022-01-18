6c9ec339690782c2aa4b59ca95fbb3583e7f68a2
Balaji Pothula <balan.pothula@gmail.com>
2022-01-18 15:05:11 +0100

    test at 2022.01.18.15.05

--- webapp_aws_iam_role_output.tf
+++ webapp_aws_iam_role_output.tf
@@ -7,31 +7,31 @@ output "webapp_aws_iam_role_arn" {
   sensitive   = false
 }
 
-output "create_date" {
+output "webapp_aws_iam_create_date" {
   value       = module.webapp_aws_iam_role.create_date
   description = "Creation date of the IAM role."
   sensitive   = false
 }
 
-output "id" {
+output "webapp_aws_iam_id" {
   value       = module.webapp_aws_iam_role.id
   description = "Name of the role."
   sensitive   = false
 }
 
-output "name" {
+output "webapp_aws_iam_name" {
   value       = module.webapp_aws_iam_role.name
   description = "Name of the role."
   sensitive   = false
 }
 
-output "webapp_aws_iam_role_tags_all" {
+output "webapp_aws_iam_webapp_aws_iam_role_tags_all" {
   value       = module.webapp_aws_iam_role.tags_all
   description = "A map of tags assigned to the resource."
   sensitive   = false
 }
 
-output "unique_id" {
+output "webapp_aws_iam_unique_id" {
   value       = module.webapp_aws_iam_role.unique_id
   description = "Stable and unique string identifying the role."
   sensitive   = false
--- webapp_aws_lambda_function_output.tf
+++ webapp_aws_lambda_function_output.tf
@@ -1,118 +1,115 @@
 # Resource  type : module
 # Module    name : webapp_aws_lambda_function
 # Attribute name : arn
-output "arn" {
+output "webapp_aws_lambda_function_arn" {
   value       = module.webapp_aws_lambda_function.arn
   description = "The ARN of the Lambda Function."
   sensitive   = false
 }
 
-# Resource  type : module
-# Module    name : webapp_aws_lambda_function
-# Attribute name : function_name
-output "function_name" {
+output "webapp_aws_lambda_function_function_name" {
   value       = module.webapp_aws_lambda_function.function_name
   description = "Unique name for your Lambda Function."
   sensitive   = false
 }
 
-output "role" {
+output "webapp_aws_lambda_function_role" {
   value       = module.webapp_aws_lambda_function.role
   description = "ARN of the function's execution role."
   sensitive   = false
 }
 
-output "code_signing_config_arn" {
+output "webapp_aws_lambda_function_code_signing_config_arn" {
   value       = module.webapp_aws_lambda_function.code_signing_config_arn
   description = "A code-signing configuration includes a set of signing profiles."
   sensitive   = false
 }
 
-output "description" {
+output "webapp_aws_lambda_function_description" {
   value       = module.webapp_aws_lambda_function.description
   description = "The description of the Lambda Function."
   sensitive   = false
 }
 
-output "handler" {
+output "webapp_aws_lambda_function_handler" {
   value       = module.webapp_aws_lambda_function.handler
   description = "The handler name of the Lambda Function."
   sensitive   = false
 }
 
-output "package_type" {
+output "webapp_aws_lambda_function_package_type" {
   value       = module.webapp_aws_lambda_function.package_type
   description = "The package type of the Lambda Function."
   sensitive   = false
 }
 
-output "memory_size" {
+output "webapp_aws_lambda_function_memory_size" {
   value       = module.webapp_aws_lambda_function.memory_size
   description = "The memory size of the Lambda Function."
   sensitive   = false
 }
 
-output "invoke_arn" {
+output "webapp_aws_lambda_function_invoke_arn" {
   value       = module.webapp_aws_lambda_function.invoke_arn
   description = "The Invoke ARN of the Lambda Function."
   sensitive   = false
 }
 
-output "last_modified" {
+output "webapp_aws_lambda_function_last_modified" {
   value       = module.webapp_aws_lambda_function.last_modified
   description = "The date Lambda Function resource was last modified."
   sensitive   = false
 }
 
-output "qualified_arn" {
+output "webapp_aws_lambda_function_qualified_arn" {
   value       = module.webapp_aws_lambda_function.qualified_arn
   description = "The ARN identifying your Lambda Function Version."
   sensitive   = false
 }
 
-output "signing_job_arn" {
+output "webapp_aws_lambda_function_signing_job_arn" {
   value       = module.webapp_aws_lambda_function.signing_job_arn
   description = "ARN of the signing job."
   sensitive   = false
 }
 
-output "signing_profile_version_arn" {
+output "webapp_aws_lambda_function_signing_profile_version_arn" {
   value       = module.webapp_aws_lambda_function.signing_profile_version_arn
   description = "ARN of the signing profile version."
   sensitive   = false
 }
 
-output "source_code_size" {
+output "webapp_aws_lambda_function_source_code_size" {
   value       = module.webapp_aws_lambda_function.source_code_size
   description = "Size in bytes of the function .zip file."
   sensitive   = false
 }
 
-output "tags_all" {
+output "webapp_aws_lambda_function_tags_all" {
   value       = module.webapp_aws_lambda_function.tags_all
   description = "A map of tags assigned to the resource, including those inherited from the provider."
   sensitive   = false
 }
 
-output "version" {
+output "webapp_aws_lambda_function_version" {
   value       = module.webapp_aws_lambda_function.version
   description = "Latest published version of your Lambda Function."
   sensitive   = false
 }
 
-output "kms_key_arn" {
+output "webapp_aws_lambda_function_kms_key_arn" {
   value       = module.webapp_aws_lambda_function.kms_key_arn
   description = "The ARN for the KMS encryption key of Lambda Function."
   sensitive   = false
 }
 
-output "source_code_hash" {
+output "webapp_aws_lambda_function_source_code_hash" {
   value       = module.webapp_aws_lambda_function.source_code_hash
   description = "Base64-encoded representation of raw SHA-256 sum of the zip file."
   sensitive   = false
 }
 
-output "tags" {
+output "webapp_aws_lambda_function_tags" {
   value       = module.webapp_aws_lambda_function.tags
   description = "The tags of Lambda Function."
   sensitive   = false
