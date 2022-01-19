7e3cc48c2833a188b9795c4049c44f902af84373
Balaji Pothula <balan.pothula@gmail.com>
2022-01-19 17:18:48 +0100

    test at 2022.01.19.17.18

--- terraform/aws/iam/policy/main.tf
+++ terraform/aws/iam/policy/main.tf
@@ -1,9 +1,8 @@
 resource "aws_iam_policy" "generic" {
 
-  description = var.description                                      # Optional argument but keep it.
-  name        = var.name                                             # Optional argument but keep it.
-  path        = var.path                                             # Optional argument but keep it.
-  policy      = null                                                 # Required argument.
-//policy      = file("${path.module}/json/AdministratorAccess.json") # Required argument.
+  description = var.description                                              # Optional argument but keep it.
+  name        = var.name                                                     # Optional argument but keep it.
+  path        = var.path                                                     # Optional argument but keep it.
+  policy      = file("${path.module}/json/AWSLambdaBasicExecutionRole.json") # Required argument.
 
 }
