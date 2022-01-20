aa456ae3a58adc9950e86c89b0355be1750e2e33
Balaji Pothula <balan.pothula@gmail.com>
2022-01-20 18:46:59 +0100

    test at 2022.01.20.18.40

--- terraform/aws/s3/bucket_object/main.tf
+++ terraform/aws/s3/bucket_object/main.tf
@@ -5,10 +5,10 @@
 # Variable  name : bucket
 resource "aws_s3_bucket_object" "generic" {
 
-  bucket        = var.bucket                    # Required argument.
-  key           = var.key                       # Required argument.
-  acl           = var.acl                       # Optional argument but keep it.
-  source        = "./python/lambda_function.py" # Optional argument but keep it.
-  tags          = var.tags                      # Optional argument but keep it.
+  bucket        = var.bucket # Required argument.
+  key           = var.key    # Required argument.
+  acl           = var.acl    # Optional argument but keep it.
+  source        = var.source # Optional argument but keep it.
+  tags          = var.tags   # Optional argument but keep it.
 
 }
