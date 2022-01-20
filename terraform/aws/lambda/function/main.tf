6d22d966837db4dca27d2e68fc1b37cfde8503d6
Balaji Pothula <balan.pothula@gmail.com>
2022-01-20 17:56:53 +0100

    flawless code commit.

--- terraform/aws/lambda/function/main.tf
+++ terraform/aws/lambda/function/main.tf
@@ -1,3 +1,28 @@
+# Data Source: aws_region
+data "aws_region" "current" {
+}
+
+# Data Source: aws_vpc
+data "aws_vpc" "default" {
+}
+
+# Data Source: aws_subnet_ids
+data "aws_subnet_ids" "available" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+# Data Source: aws_security_groups
+data "aws_security_groups" "default" {
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+  filter {
+    name   = "description"
+    values = ["default VPC security group"]
+  }
+}
+
 # Resource  type : aws_lambda_function
 # Resource  name : generic
 # Attribute name : function_name
