df9cdced212c2c9a94aeffb11b4c23855bbb0452
Balaji Pothula <balan.pothula@gmail.com>
2022-01-23 12:21:28 +0100

    test at 2022.01.23.12.25

--- terraform/aws/rds/cluster/main.tf
+++ terraform/aws/rds/cluster/main.tf
@@ -8,11 +8,6 @@ data "aws_availability_zones" "available" {
   state                  = "available"
 
   filter {
-    name   = "zone-name"
-    values = ["availability-zone"]
-  }
-
-  filter {
     name   = "zone-type"
     values = ["availability-zone"]
   }
