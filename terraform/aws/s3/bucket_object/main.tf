17074f34685ee60645cf2a73f998389f342d9eec
Balaji Pothula <balan.pothula@gmail.com>
2022-01-23 12:27:03 +0100

    test at 2022.01.23.12.30

--- terraform/aws/s3/bucket_object/main.tf
+++ terraform/aws/s3/bucket_object/main.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket_object" "generic" {
   bucket        = var.bucket  # Required argument.
   key           = var.key     # Required argument.
   acl           = var.acl     # Optional argument but keep it.
-  content       = var.content # Optional argument but keep it.
+//content       = var.content # Optional argument but keep it.
   etag          = var.etag    # Optional argument but keep it.
   tags          = var.tags    # Optional argument but keep it.
 
