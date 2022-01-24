5af6944a7943d9783c04710b89c0363125bce8a0
Balaji Pothula <balan.pothula@gmail.com>
2022-01-24 18:33:50 +0100

    flawless code commit.

--- main.tf
+++ main.tf
@@ -92,8 +92,8 @@ module "webapp_aws_s3_bucket_object" {
   bucket      = module.webapp_aws_s3_bucket.id                               # Required argument.
   key         = "/${local.yyyy}/${local.mm}/${local.dd}/lambda_function.zip" # Required argument.
   acl         = "private"                                                    # Optional argument but keep it.
-  source_code = data.archive_file.webapp.output_path                         # Optional argument but keep it.
   etag        = filemd5(data.archive_file.webapp.output_path)                # Optional argument but keep it.
+  source_code = data.archive_file.webapp.output_path                         # Optional argument but keep it.
   tags       = {                                                             # Optional argument but keep it.
     "AppName"        = "WebApp"
   }
--- terraform/aws/s3/bucket_object/main.tf
+++ terraform/aws/s3/bucket_object/main.tf
@@ -5,12 +5,11 @@
 # Variable  name : bucket
 resource "aws_s3_bucket_object" "generic" {
 
-  bucket  = var.bucket  # Required argument.
-  key     = var.key     # Required argument.
-  acl     = var.acl     # Optional argument but keep it.
-//etag    = var.etag # Optional argument but keep it.
-//source  = "./lambda_function.zip" # Optional argument but keep it.
+  bucket  = var.bucket      # Required argument.
+  key     = var.key         # Required argument.
+  acl     = var.acl         # Optional argument but keep it.
+  etag    = var.etag        # Optional argument but keep it.
   source  = var.source_code # Optional argument but keep it.
-  tags    = var.tags    # Optional argument but keep it.
+  tags    = var.tags        # Optional argument but keep it.
 
 }
