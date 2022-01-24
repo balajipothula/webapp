af4b091e2993e42d8aae89196125631220e9a5ec
Balaji Pothula <balan.pothula@gmail.com>
2022-01-24 18:08:40 +0100

    test at 2022.01.24.18.05

--- terraform/aws/s3/bucket_object/main.tf
+++ terraform/aws/s3/bucket_object/main.tf
@@ -8,8 +8,8 @@ resource "aws_s3_bucket_object" "generic" {
   bucket  = var.bucket  # Required argument.
   key     = var.key     # Required argument.
   acl     = var.acl     # Optional argument but keep it.
-  etag    = var.etag # Optional argument but keep it.
-  source  = "./lambda_function.zip"          # Optional argument but keep it.
+//etag    = var.etag # Optional argument but keep it.
+  source  = "./lambda_function.zip" # Optional argument but keep it.
   tags    = var.tags    # Optional argument but keep it.
 
 }
