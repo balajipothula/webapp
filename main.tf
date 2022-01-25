5fd330f3b37831630617e8f1fe1091cbc0f95804
Balaji Pothula <balan.pothula@gmail.com>
2022-01-25 16:56:41 +0100

    test at 2022.01.25.16.55

--- main.tf
+++ main.tf
@@ -223,3 +223,36 @@ resource "aws_apigatewayv2_api" "webapp" {
   protocol_type = "HTTP"
 
 }
+
+resource "aws_apigatewayv2_stage" "webapp" {
+
+  depends_on = [
+    aws_apigatewayv2_api.webapp,
+  ]
+
+  api_id = aws_apigatewayv2_api.webapp.id
+
+  name        = "webapp"
+  auto_deploy = true
+
+  access_log_settings {
+
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+
+  }
+
+}
