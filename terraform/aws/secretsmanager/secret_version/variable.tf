87691bc066a47633535166532306523ccf862e23
Balaji Pothula <balan.pothula@gmail.com>
2022-01-28 13:02:38 +0100

    test at 2022.01.28.13.02

--- terraform/aws/secretsmanager/secret_version/variable.tf
+++ terraform/aws/secretsmanager/secret_version/variable.tf
@@ -22,7 +22,7 @@ variable "secret_string" {
   }
   description = "Specifies text data that you want to encrypt and store in this version of the secret." 
   validation {
-    condition     = var.secret_string["dbInstanceIdentifier"] != null && 3 <= length(var.secret_string["dbInstanceIdentifier"]) && length(var.tags["dbInstanceIdentifier"]) <= 64  && length(regexall("[^A-Za-z0-9-]", var.tags["dbInstanceIdentifier"])) == 0
+    condition     = var.secret_string["dbInstanceIdentifier"] != null && 3 <= length(var.secret_string["dbInstanceIdentifier"]) && length(var.secret_string["dbInstanceIdentifier"]) <= 64  && length(regexall("[^A-Za-z0-9-]", var.secret_string["dbInstanceIdentifier"])) == 0
     error_message = "Error: secret_string of dbInstanceIdentifier must not null, length must be in between 3 to 64 and only contain alphabets, numbers, and hyphens."
   }
   validation {
