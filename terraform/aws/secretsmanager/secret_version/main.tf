054c370cfc21e6e042dcb379ebffc0ab99cc9961
Balaji Pothula <balan.pothula@gmail.com>
2022-01-28 13:14:39 +0100

    test at 2022.01.28.13.14

--- terraform/aws/secretsmanager/secret_version/main.tf
+++ terraform/aws/secretsmanager/secret_version/main.tf
@@ -5,6 +5,6 @@
 resource "aws_secretsmanager_secret_version" "generic" {
 
   secret_id     = var.secret_id                 # Required argument.
-  secret_string = jsonencode(var.secret_string) # Optional argument, but required if secret_binary is not set.
+  secret_string = var.secret_string # Optional argument, but required if secret_binary is not set.
 
 }
--- terraform/aws/secretsmanager/secret_version/variable.tf
+++ terraform/aws/secretsmanager/secret_version/variable.tf
@@ -10,27 +10,7 @@ variable "secret_id" {
 }
 
 variable "secret_string" {
-
-  type = jsonencode(map(string))
-/*
-  default = jsonencode({
-    "dbInstanceIdentifier" = "webapp"
-    "engine"               = "aurora-postgresql"
-    "host"                 = "webapp.cluster-cxxn79rkxpwp.eu-central-1.rds.amazonaws.com"
-    "port"                 = "5432"
-    "resourceId"           = "cluster-BSINHSF55FNPNAG72B7GPHH4B4"
-    "username"             = "webapp"
-    "password"             = "WebApp#2022"
-  })
+  default     = null
   description = "Specifies text data that you want to encrypt and store in this version of the secret." 
-  validation {
-    condition     = var.secret_string["dbInstanceIdentifier"] != null && 3 <= length(var.secret_string["dbInstanceIdentifier"]) && length(var.secret_string["dbInstanceIdentifier"]) <= 64  && length(regexall("[^A-Za-z0-9-]", var.secret_string["dbInstanceIdentifier"])) == 0
-    error_message = "Error: secret_string of dbInstanceIdentifier must not null, length must be in between 3 to 64 and only contain alphabets, numbers, and hyphens."
-  }
-  validation {
-    condition     = var.secret_string != null && 0 < length(var.secret_string) && length(var.secret_string) < 33
-    error_message = "Error: secret_string at least one or more expected upto 50."
-  }
-*/
-  sensitive = true
+  sensitive   = true
 }
