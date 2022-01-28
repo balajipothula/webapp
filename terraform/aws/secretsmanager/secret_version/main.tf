d16a81887db71edd6f91536735d3b4d4a1131a1c
Balaji Pothula <balan.pothula@gmail.com>
2022-01-28 13:00:57 +0100

    test at 2022.01.28.13.00

--- terraform/aws/secretsmanager/secret_version/main.tf
+++ terraform/aws/secretsmanager/secret_version/main.tf
@@ -4,7 +4,7 @@
 # Variable name : secret_id
 resource "aws_secretsmanager_secret_version" "generic" {
 
-  secret_id     = var.secret_id     # Required argument.
-  secret_string = var.secret_string # Optional argument, but required if secret_binary is not set.
+  secret_id     = var.secret_id                 # Required argument.
+  secret_string = jsonencode(var.secret_string) # Optional argument, but required if secret_binary is not set.
 
 }
