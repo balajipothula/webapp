b223338c15c81496ea017043257147e78060fccf
Balaji Pothula <balan.pothula@gmail.com>
2022-01-31 18:13:18 +0100

    flawless code commit.

--- postgresql/webapp_db/public/2022.01.31.ChangeLog.sql
+++ postgresql/webapp_db/public/2022.01.31.ChangeLog.sql
@@ -7,3 +7,11 @@ CREATE TABLE IF NOT EXISTS webapp_db.public."Login"(
   "username" VARCHAR   UNIQUE NOT NULL,
   "password" VARCHAR   NOT NULL
 );
+
+-- comment: Please do not change the value of author:changsetid
+-- changeset BalajiPothula:2022-01-31T17:12:00Z
+CREATE TABLE IF NOT EXISTS webapp_db.public."Logout"(
+  "id"       BIGSERIAL PRIMARY KEY,
+  "username" VARCHAR   UNIQUE NOT NULL,
+  "password" VARCHAR   NOT NULL
+);
