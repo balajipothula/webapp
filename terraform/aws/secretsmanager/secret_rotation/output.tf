43b436b1eed2727ff2420d2e91883fc17cff85fc
Balaji Pothula <balan.pothula@gmail.com>
2022-02-02 19:09:00 +0100

    test at 2022.02.02.19.08

--- terraform/aws/secretsmanager/secret_rotation/output.tf
+++ terraform/aws/secretsmanager/secret_rotation/output.tf
@@ -1,14 +1,14 @@
 # Resource  type : aws_secretsmanager_secret_version
 # Resource  name : generic
-# Attribute name : arn
-output "arn" {
-  value       = aws_secretsmanager_secret_rotation.generic.arn
+# Attribute name : id
+output "id" {
+  value       = aws_secretsmanager_secret_rotation.generic.id
   description = "The ARN of the secret."
   sensitive   = false
 }
-
-output "id" {
-  value       = aws_secretsmanager_secret_rotation.generic.id
+/*
+output "arn" {
+  value       = aws_secretsmanager_secret_rotation.generic.arn
   description = "The ARN of the secret."
   sensitive   = false
 }
@@ -18,3 +18,4 @@ output "rotation_enabled" {
   description = "Specifies whether automatic rotation is enabled for this secret."
   sensitive   = false
 }
+*/
