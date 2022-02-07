6c09b99d1c37d91b53511a141fdc0c29b06b97ce
Balaji Pothula <balan.pothula@gmail.com>
2022-02-07 13:36:48 +0100

    test at 2022.02.07.13.36

--- main.tf
+++ main.tf
@@ -166,10 +166,10 @@ module "webapp_aws_s3_bucket" {
   acl    = "private"                             # Optional argument, but keep it.
   policy = file("./json/WebAppS3IAMPolicy.json") # Optional argument, but keep it.
   tags   = {                                     # Optional argument, but keep it.
-    "Name"            = "webapp"
-    "AppName"         = "FastAPI Web App"
+    "Name"            = "yousician"
+    "AppName"         = "Yousician Web App"
     "DeveloperName"   = "Balaji Pothula"
-    "DeveloperEmail"  = "balaji.pothula@techie.com"
+    "DeveloperEmail"  = "balaji@yousician.com"
   }
 
 }
@@ -189,10 +189,10 @@ module "webapp_aws_s3_bucket_object" {
   etag        = filemd5(data.archive_file.webapp.output_path) # Optional argument, but keep it.
   source_code = data.archive_file.webapp.output_path          # Optional argument, but keep it.
   tags        = {                                             # Optional argument, but keep it.
-    "Name"            = "webapp"
-    "AppName"         = "FastAPI Web App"
+    "Name"            = "yousician"
+    "AppName"         = "Yousician Web App"
     "DeveloperName"   = "Balaji Pothula"
-    "DeveloperEmail"  = "balaji.pothula@techie.com"
+    "DeveloperEmail"  = "balaji@yousician.com"
   }
 
 }
@@ -240,10 +240,10 @@ module "webapp_aws_lambda_function" {
   s3_bucket                      = module.webapp_aws_s3_bucket.id               # Optional argument, but keep it.
   s3_key                         = "${local.yyyymmdd}/${local.webapp_zip}"      # Optional argument, conflicts with filename and image_uri.
   tags                           = {                                            # Optional argument, but keep it.
-    "Name"            = "webapp"
-    "AppName"         = "FastAPI Web App"
+    "Name"            = "yousician"
+    "AppName"         = "Yousician Web App"
     "DeveloperName"   = "Balaji Pothula"
-    "DeveloperEmail"  = "balaji.pothula@techie.com"
+    "DeveloperEmail"  = "balaji@yousician.com"
   }
   timeout                        = 60                                           # Optional argument, but keep it.
 
@@ -261,10 +261,10 @@ module "webapp_aws_cloudwatch_log_group" {
   name              = "/aws/lambda/${module.webapp_aws_lambda_function.function_name}" # Optional argument, but keep it.
   retention_in_days = 14                                                               # Optional argument, but keep it.
   tags              = {                                                                # Optional argument, but keep it.
-    "Name"            = "webapp"
-    "AppName"         = "FastAPI Web App"
+    "Name"            = "yousician"
+    "AppName"         = "Yousician Web App"
     "DeveloperName"   = "Balaji Pothula"
-    "DeveloperEmail"  = "balaji.pothula@techie.com"
+    "DeveloperEmail"  = "balaji@yousician.com"
   }
 
 }
@@ -403,10 +403,10 @@ module "webapp_aws_rds_cluster" {
   skip_final_snapshot          = true                                   # Optional argument, but keep it.
   storage_encrypted            = true                                   # Optional argument, but keep it.
   tags                         = {                                      # Optional argument, but keep it.
-    "Name"            = "webapp"
-    "AppName"         = "FastAPI Web App"
+    "Name"            = "yousician"
+    "AppName"         = "Yousician Web App"
     "DeveloperName"   = "Balaji Pothula"
-    "DeveloperEmail"  = "balaji.pothula@techie.com"
+    "DeveloperEmail"  = "balaji@yousician.com"
   }
 
 }
@@ -426,10 +426,10 @@ module "webapp_aws_secretsmanager_secret" {
   name                           = "webapp"                 # Optional argument, conflicts with name_prefix.
   recovery_window_in_days        = 7                        # Optional argument, but keep it.
   tags                           = {                        # Optional argument, but keep it.
-    "Name"            = "webapp"
-    "AppName"         = "FastAPI Web App"
+    "Name"            = "yousician"
+    "AppName"         = "Yousician Web App"
     "DeveloperName"   = "Balaji Pothula"
-    "DeveloperEmail"  = "balaji.pothula@techie.com"
+    "DeveloperEmail"  = "balaji@yousician.com"
   }
 
 }
@@ -477,9 +477,9 @@ module "webapp_aws_vpc_endpoint" {
   security_group_ids  = data.aws_security_groups.default.ids        # Optional argument, but required for endpoints of type Interface.
   tags                = {                                           # Optional argument, but keep it.
     "Name"            = "webapp_secretsmanager"
-    "AppName"         = "FastAPI Web App"
+    "AppName"         = "Yousician Web App"
     "DeveloperName"   = "Balaji Pothula"
-    "DeveloperEmail"  = "balaji.pothula@techie.com"
+    "DeveloperEmail"  = "balaji@yousician.com"
   }
   vpc_endpoint_type   = "Interface"                                 # Optional argument, but keep it.
 
