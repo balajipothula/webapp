4f534a2243aa55927f5e417bd1064b9a59218634
Balaji Pothula <balan.pothula@gmail.com>
2022-02-13 15:52:13 +0100

    test at 2022.02.13.15.52

--- python/src/yousician_lambda_function.py
+++ python/src/yousician_lambda_function.py
@@ -218,5 +218,13 @@ async def getSongsSearch(parameter: str):
   values = { "artist": parameter, "title": parameter }
   return await database.fetch_all(query = query, values = values)
 
+@app.get("/songs/avg/difficulty")
+async def getSongsAvgDifficulty(level: int = 0):
+  query  = """ SELECT AVG("difficulty")::numeric(10,2) AS "avgDifficulty" FROM yousician_db.public."Song" """
+  values = None
+  if 0 < level:
+    query  = """SELECT AVG("difficulty")::numeric(10,2) AS "avgDifficulty" FROM yousician_db.public."Song" WHERE "level" = :level"""
+    values = { "level": level }
+  return await database.fetch_all(query = query, values = values)
 
 lambda_handler = Mangum(app)
