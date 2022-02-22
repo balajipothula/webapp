3c4e241224664df7284562ac1f38e6fb8b09e016
Balaji Pothula <balan.pothula@gmail.com>
2022-02-22 11:32:20 +0100

    test

--- python/src/yousician_lambda_function.py
+++ python/src/yousician_lambda_function.py
@@ -77,7 +77,7 @@ def getCredentials(region: str, secret: str) -> dict:
     elif exception == "InvalidParameterException":
       logger.error("The request had invalid parameters: ", clientError)
     elif exception == "InvalidRequestException":
-      logger.error("The request was invalid due to :", clientError)
+      logger.error("The request was invalid due to :")
     elif exception == "ResourceNotFoundException":
       logger.error("The requested secret " + secret + " was not found")
   else:
