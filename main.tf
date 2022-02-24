30a6a853a2a583c263c8718533ac6011ecbd429c
Balaji Pothula <balan.pothula@gmail.com>
2022-02-24 13:36:38 +0100

    test at 2022.02.24.13.36

--- main.tf
+++ main.tf
@@ -157,7 +157,7 @@ module "webapp_aws_instance" {
   instance_type                        = "t2.micro"              # Optional argument, but keep it.
 //ipv6_address_count                   = 1                       # Optional argument, but keep it.
   monitoring                           = false                   # Optional argument, but keep it.
-  subnet_id                            = "subnet-a54b1ecf"       # Optional argument, but keep it.
+//subnet_id                            = "subnet-a54b1ecf"       # Optional argument, but keep it.
   tags                                 = {                       # Optional argument, but keep it.
     "Name"           = "WebApp"
     "AppName"        = "Python FastAPI Web App"
