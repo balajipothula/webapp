db1f9f70aabb8221278f5e725c447a2eb9e39fa8
Balaji Pothula <balan.pothula@gmail.com>
2022-02-26 18:08:28 +0100

    test at 2022.02.26.18.08

--- main.tf
+++ main.tf
@@ -175,16 +175,19 @@ module "webapp_aws_instance" {
   instance_type                        = "t2.micro"              # Optional argument, but keep it.
 //ipv6_address_count                   = 1                       # Optional argument, but keep it.
   monitoring                           = false                   # Optional argument, but keep it.
-/*
+
   root_block_device                    = [
     {                                                            # Optional block, but keep it.
       delete_on_termination            = true                    # Optional block argument, but keep it.
+      device_name                      = "/dev/xvda"             # Optional block argument, but keep it.
       encrypted                        = false                   # Optional block argument, but keep it.
+      iops                             = 100                     # Optional block argument, but keep it.
+      throughput                       = 0                       # Optional block argument, but keep it.
       volume_size                      = 8                       # Optional block argument, but keep it.
       volume_type                      = "gp2"                   # Optional block argument, but keep it.
     }
   ]
-*/
+
   security_groups                      = [                       # Optional argument, but keep it.
     "default",
   ]
