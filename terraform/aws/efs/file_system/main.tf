5f7443103a87f6d4c1c5e412d09c7dd4c5bef969
Balaji Pothula <balan.pothula@gmail.com>
2022-02-27 19:56:30 +0100

    test at 2022.02.27.19.56

--- terraform/aws/efs/file_system/main.tf
+++ terraform/aws/efs/file_system/main.tf
@@ -5,7 +5,7 @@
 # Variable  name : availability_zone_name
 resource "aws_efs_file_system" "generic" {
 
-  availability_zone_count               = 3                                       # Optional argument, but keep it.
+  count                                 = 3                                       # Optional argument, but keep it.
 
   availability_zone_name                = var.availability_zone_name              # Optional argument, but keep it.
   creation_token                        = var.creation_token                      # Optional argument, but keep it.
