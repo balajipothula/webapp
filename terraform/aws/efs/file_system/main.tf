090af4b798b805b936c15e12cbbb2e6f55d94133
Balaji Pothula <balan.pothula@gmail.com>
2022-02-27 18:30:07 +0100

    test at 2022.02.27.18.30

--- terraform/aws/efs/file_system/main.tf
+++ terraform/aws/efs/file_system/main.tf
@@ -9,13 +9,15 @@ resource "aws_efs_file_system" "generic" {
   creation_token                        = var.creation_token                      # Optional argument, but keep it.
   encrypted                             = var.encrypted                           # Optional argument, but keep it.
 //kms_key_id                            = var.kms_key_id                          # Optional argument, but required if encrypted set true.
+/*
   lifecycle_policy {                                                              # Optional block, but keep it.
     transition_to_ia                    = var.transition_to_ia                    # Optional block argument, but keep it.
     transition_to_primary_storage_class = var.transition_to_primary_storage_class # Optional block argument, but keep it.
   }
+*/
   performance_mode                      = var.performance_mode                    # Optional argument, but keep it.
-  provisioned_throughput_in_mibps       = var.provisioned_throughput_in_mibps     # Optional argument, but only applicable with throughput_mode set to provisioned.
+//provisioned_throughput_in_mibps       = var.provisioned_throughput_in_mibps     # Optional argument, but only applicable with throughput_mode set to provisioned.
   tags                                  = var.tags                                # Optional argument, but keep it.
-  throughput_mode                       = var.throughput_mode                     # Optional argument, if value is provisioned, it will impact provisioned_throughput_in_mibps.
+//throughput_mode                       = var.throughput_mode                     # Optional argument, if value is provisioned, it will impact provisioned_throughput_in_mibps.
 
 }
