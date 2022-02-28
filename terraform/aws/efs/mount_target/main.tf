bee6d668612ab0da3155c6c42f56502209ed69fe
Balaji Pothula <balan.pothula@gmail.com>
2022-02-28 17:46:15 +0100

    test at 2022.02.28.17.46

--- terraform/aws/efs/mount_target/main.tf
+++ terraform/aws/efs/mount_target/main.tf
@@ -5,6 +5,8 @@
 # Variable  name : file_system_id
 resource "aws_efs_mount_target" "generic" {
 
+  count           = 2
+
   file_system_id  = var.file_system_id  # Required argument.
   subnet_id       = var.subnet_id       # Required argument.
 //ip_address      = var.ip_address      # Optional argument, but keep it.
