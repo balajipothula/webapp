8c7957e23f8a3a9a4a25e75e4e9c6941a57316e7
Balaji Pothula <balan.pothula@gmail.com>
2022-02-28 18:54:43 +0100

    test at 2022.02.28.18.54

--- main.tf
+++ main.tf
@@ -186,24 +186,6 @@ module "webapp_aws_efs_file_system" {
 
 
 # Mounting of AWS EFS (Elastic File System) target for WebApp.
-module "webapp_aws_efs_mount_target_2" {
-
-  source          = "./terraform/aws/efs/mount_target"
-
-  depends_on      = [
-    module.webapp_aws_efs_file_system,
-  ]
-
-
-  file_system_id  = module.webapp_aws_efs_file_system.id # Required argument.
-  subnet_id       = "subnet-9fa323e3"                    # Required argument.
-//ip_address      = var.ip_address                       # Optional argument, but keep it.
-  security_groups = data.aws_security_groups.default.ids # Optional argument, but keep it.
-
-}
-
-
-# Mounting of AWS EFS (Elastic File System) target for WebApp.
 module "webapp_aws_efs_mount_target" {
 
   source          = "./terraform/aws/efs/mount_target"
@@ -212,7 +194,24 @@ module "webapp_aws_efs_mount_target" {
     module.webapp_aws_efs_file_system,
   ]
 
-  for_each        = toset(["subnet-a54b1ecf"])
+  file_system_id  = module.webapp_aws_efs_file_system.id # Required argument.
+  subnet_id       = "subnet-a54b1ecf"                    # Required argument.
+//ip_address      = var.ip_address                       # Optional argument, but keep it.
+  security_groups = data.aws_security_groups.default.ids # Optional argument, but keep it.
+
+}
+
+/*
+# Mounting of AWS EFS (Elastic File System) target for WebApp.
+module "webapp_aws_efs_mount_target_2" {
+
+  source          = "./terraform/aws/efs/mount_target"
+
+  depends_on      = [
+    module.webapp_aws_efs_file_system,
+  ]
+
+  for_each        = toset(["subnet-9fa323e3"])
 
   file_system_id  = module.webapp_aws_efs_file_system.id # Required argument.
   subnet_id       = each.key                             # Required argument.
@@ -220,6 +219,7 @@ module "webapp_aws_efs_mount_target" {
   security_groups = data.aws_security_groups.default.ids # Optional argument, but keep it.
 
 }
+*/
 
 # Creation of AWS EC2 (Elastic Compute Cloud) Instance for WebApp.
 module "webapp_aws_instance" {
