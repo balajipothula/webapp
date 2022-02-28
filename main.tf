b29d18a753357f685d860f90fbe36752a5d85a75
Balaji Pothula <balan.pothula@gmail.com>
2022-02-28 18:38:44 +0100

    test at 2022.02.28.18.38

--- main.tf
+++ main.tf
@@ -184,7 +184,24 @@ module "webapp_aws_efs_file_system" {
 
 }
 
+/*
+# Mounting of AWS EFS (Elastic File System) target for WebApp.
+module "webapp_aws_efs_mount_target_2" {
 
+  source          = "./terraform/aws/efs/mount_target"
+
+  depends_on      = [
+    module.webapp_aws_efs_file_system,
+  ]
+
+
+  file_system_id  = module.webapp_aws_efs_file_system.id # Required argument.
+  subnet_id       = "subnet-9fa323e3"                    # Required argument.
+//ip_address      = var.ip_address                       # Optional argument, but keep it.
+  security_groups = data.aws_security_groups.default.ids # Optional argument, but keep it.
+
+}
+*/
 
 # Mounting of AWS EFS (Elastic File System) target for WebApp.
 module "webapp_aws_efs_mount_target" {
@@ -195,11 +212,9 @@ module "webapp_aws_efs_mount_target" {
     module.webapp_aws_efs_file_system,
   ]
 
-//for_each        = toset(["subnet-a54b1ecf", "subnet-9fa323e3"])
-  for_each        = toset(["subnet-9fa323e3"])
+  for_each        = toset(["subnet-a54b1ecf"])
 
   file_system_id  = module.webapp_aws_efs_file_system.id # Required argument.
-//subnet_id       = "subnet-a54b1ecf"                    # Required argument.
   subnet_id       = each.key                             # Required argument.
 //ip_address      = var.ip_address                       # Optional argument, but keep it.
   security_groups = data.aws_security_groups.default.ids # Optional argument, but keep it.
