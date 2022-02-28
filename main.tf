0f28d99aebb0d2ae8ffaf168056fb4f43b9ef8b2
Balaji Pothula <balan.pothula@gmail.com>
2022-02-28 15:01:16 +0100

    test at 2022.02.28.15.01

--- main.tf
+++ main.tf
@@ -201,7 +201,7 @@ module "webapp_aws_efs_mount_target" {
   file_system_id  = "webapp" # Required argument.
 
 //file_system_id  = module.webapp_aws_efs_file_system.id # Required argument.
-  subnet_id       = "data.aws_subnets.available.ids.${count.index}" # Required argument.
+  subnet_id       = "${element(aws_subnet.default.*.id, 3)}"
 
 //ip_address      = var.ip_address                       # Optional argument, but keep it.
   security_groups = data.aws_security_groups.default.ids # Optional argument, but keep it.
