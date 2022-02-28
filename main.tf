c1bf74c76f528423c1d86f2fa9b336bf427d0c6c
Balaji Pothula <balan.pothula@gmail.com>
2022-02-28 17:25:04 +0100

    test at 2022.02.28.17.24

--- main.tf
+++ main.tf
@@ -196,10 +196,8 @@ module "webapp_aws_efs_mount_target" {
   file_system_id  = module.webapp_aws_efs_file_system.id # Required argument.
 //subnet_id       = "subnet-a54b1ecf"                    # Required argument.
 
-//Testing block begin.
   for_each        = toset(["subnet-a54b1ecf"])
   subnet_id       = each.key                    
-//testing block end.
 
 //ip_address      = var.ip_address                       # Optional argument, but keep it.
   security_groups = data.aws_security_groups.default.ids # Optional argument, but keep it.
