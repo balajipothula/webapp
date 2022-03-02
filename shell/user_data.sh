d8977bd648b61ce30c21f3da37d85792eb4e2a4f
Balaji Pothula <balan.pothula@gmail.com>
2022-03-02 19:02:12 +0100

    test at 2022.03.02.19.02

--- shell/user_data.sh
+++ shell/user_data.sh
@@ -32,5 +32,8 @@ mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2
 # change permissions of root directory.
 chmod 777 /home/ec2-user/root
 
+# pull Jenkins supported Docker image.
+docker pull balajipothula/jenkins:openjdk8
+
 # run Jenkins.
 docker run --name jenkins -d -i --privileged -p 8080:8080 -v $HOME/root/.tomcat:/root/.tomcat -v $HOME/root/.jenkins:/root/.jenkins -v $HOME/root/.m2:/root/.m2 balajipothula/jenkins:openjdk8
