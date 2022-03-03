8f72a7a8731e0f65c890ee43da665059a327e6a0
Balaji Pothula <balan.pothula@gmail.com>
2022-03-03 19:10:43 +0100

    test at 2022.03.03.19.10

--- main.tf
+++ main.tf
@@ -781,13 +781,13 @@ module "webapp_aws_ecs_cluster" {
     }]
 
   }]
-*/
+
   default_capacity_provider_strategy = [{       # Optional configuration block.
     capacity_provider = "webapp"                # Required block argument.
     weight            = 50                      # Optional block argument.
     base              = 3                       # Optional block argument.
   }]
-
+*/
   setting                            = [{       # Optional configuration block, but keep it.
     name  = "containerInsights"                 # Required block argument.
     value = "disabled"                          # Required block argument.
