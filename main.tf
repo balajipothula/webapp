26f5ce7b9ec4885d66942649fbd480039dc2b430
Balaji Pothula <balan.pothula@gmail.com>
2022-03-03 19:00:44 +0100

    test at 2022.03.03.19.00

--- main.tf
+++ main.tf
@@ -728,7 +728,6 @@ module "webapp_aws_vpc_endpoint" {
   vpc_endpoint_type   = "Interface"                                                    # Optional argument, but keep it.
 
 }
-*/
 
 # Creation of AWS ECR (Elastic Container Registry) Repository for WebApp.
 module "webapp_aws_ecr_repository" {
@@ -756,6 +755,7 @@ module "webapp_aws_ecr_repository" {
   }
 
 }
+*/
 
 # Creation of AWS ECS  (Elastic Container Service) Cluster for WebApp.
 module "webapp_aws_ecs_cluster" {
