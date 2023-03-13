d7328de091bf845cbf832020d0fbb8c173765f4f
Balaji Pothula <balan.pothula@gmail.com>
2023-03-13 19:55:09 +0100

    Updated github provider.

--- version.tf
+++ version.tf
@@ -24,6 +24,11 @@ terraform {
       version = "~> 3.55.0"
     }
 
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.3.0"
+    }
+
   }
 
   required_version = ">= 1.0.5"
