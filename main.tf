0b207157e9052a03f00f235191662617280c9308
Balaji Pothula <balan.pothula@gmail.com>
2025-07-01 18:37:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -643,7 +643,7 @@ module "webapp_aws_rds_cluster" {
   deletion_protection          = false                                     # Optional argument, but keep it.
   enable_http_endpoint         = true                                      # Optional argument, but keep it.
   engine                       = "aurora-postgresql"                       # Optional argument, but keep it.
-  engine_mode                  = "serverless-v2"                           # Optional argument, but keep it.
+ #engine_mode                  = "serverless-v2"                           # Optional argument, comment it.
   engine_version               = "16.1"                                    # Optional argument, but keep it.
   final_snapshot_identifier    = "webapp-snapshot-at-${local.datetime}"    # Optional argument, but keep it.
   master_password              = var.master_password                       # Required argument.
