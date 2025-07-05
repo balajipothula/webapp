707796df1139baff3761843874c05540e68ef663
Balaji Pothula <balan.pothula@gmail.com>
2025-07-05 17:01:03 +0530

    Deleted data blocks from main.tf

--- main.tf
+++ main.tf
@@ -5,11 +5,8 @@ provider "github" {
   token = var.token_github
 
 }
-
-data "github_actions_secrets" "webapp" {
-  full_name = "balajipothula/webapp"
-}
 */
+
 # terraform provider information.
 provider "aws" {
 
@@ -18,101 +15,8 @@ provider "aws" {
   secret_key = var.secret_key
 
 }
-/*
-# Data Source: aws_region
-data "aws_region" "current" {}
-
-# Data Source: aws_caller_identity
-data "aws_caller_identity" "current" {}
-
-# Data Source: aws_partition
-data "aws_partition" "current" {}
-
-# Data Source: aws_vpc
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Data Source: aws_availability_zones
-data "aws_availability_zones" "available" {
-
-  all_availability_zones = true
-  state                  = "available"
-
-  filter {
-    name   = "zone-type"
-    values = ["availability-zone"]
-  }
-
-}
-
-# Data Source: aws_subnet_ids
-data "aws_subnet_ids" "available" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-# Data Source: aws_subnet
-data "aws_subnet" "default" {
-  for_each = data.aws_subnet_ids.available.ids
-  id       = each.value
-}
-
-# Data Source: aws_security_groups
-data "aws_security_groups" "default" {
-
-  filter {
-    name   = "group-name"
-    values = ["default"]
-  }
-
-  filter {
-    name   = "description"
-    values = ["default VPC security group"]
-  }
-
-}
 
 /*
-data "aws_ami" "default" {
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["amzn-ami-hvm-*-x86_64-gp2"]
-  }
-
-  most_recent = "true"
-  owners      = ["self"]
-  owners      = ["amazon"]
-
-}
-*/
-/*
-# Archive Lambda Function source code.
-data "archive_file" "webapp" {
-  type        = "zip"
-  source_file = local.webapp_src
-  output_path = "./${local.webapp_zip}"
-}
-
-locals {
-  timestamp  = timestamp()
-  yyyymmdd   = formatdate("YYYY/MM/DD",          local.timestamp)   
-  datetime   = formatdate("YYYY-MM-DD-hh-mm-ss", local.timestamp)
-  layer_zip  = "./python/lib/layer.zip"
-  webapp_src = "./python/src/lambda_function.py"
-  webapp_zip = "webapp-${local.datetime}.zip"
-}
-*/
 # AWS Default Security Group Update.
 resource "aws_default_security_group" "update" {
 
@@ -183,6 +87,7 @@ resource "aws_default_security_group" "update" {
   }
 
 }
+*/
 
 /*
 # Creation of AWS EFS (Elastic File System) for WebApp.
@@ -338,27 +243,6 @@ module "webapp_aws_iam_role_policy_attachment" {
 
   role       = module.webapp_aws_iam_role.name  # Required argument.
   policy_arn = module.webapp_aws_iam_policy.arn # Required argument.
-
-}
-*/
-
-/*
-data "aws_iam_policy_document" "webapp_lambda_src_s3_bucket_policy" {
-
-  statement {
-    sid = "WebAppLambdaSrcS3BucketPolicy"
-    effect = "Allow"
-    principals {
-      type        = "AWS"
-      identifiers = ["${data.aws_caller_identity.current.arn}"]
-    }
-    actions = [
-      "s3:GetObject"
-    ]
-    resources = [
-      "arn:aws:s3:::webapp-aws-lambda-src-s3-bucket-12/*"
-    ]
-  }
 
 }
 */
