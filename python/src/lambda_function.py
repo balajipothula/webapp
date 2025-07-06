fa4c77227948b7bca8d7e50910009f4374f942cd
Balaji Pothula <balan.pothula@gmail.com>
2025-07-06 17:47:00 +0530

    Debugging Lambda

--- python/src/lambda_function.py
+++ python/src/lambda_function.py
@@ -1,14 +1,19 @@
+import logging
 from fastapi import FastAPI
 from mangum import Mangum
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 app = FastAPI()
 
 @app.get("/")
 def root():
+    logger.info("Root endpoint hit.")
     return {"message": "Hello from Lambda"}
 
-# Create a Mangum adapter
 adapter = Mangum(app)
 
 def lambda_handler(event, context):
+    logger.info(f"Event received: {event}")
     return adapter(event, context)
