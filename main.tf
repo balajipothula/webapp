4859617ad4099446487299c2ff4cbd2c1e742952
Balaji Pothula <balan.pothula@gmail.com>
2025-07-06 16:59:57 +0530

    runtime python3.7

--- main.tf
+++ main.tf
@@ -134,7 +134,7 @@ module "webapp_aws_lambda_layer_version" {
 
   layer_name               = "webapp"                          # Required argument.
   compatible_architectures = ["arm64", "x86_64"]               # Optional argument, but keep it.
-  compatible_runtimes      = ["python3.9"]                     # Optional argument, but keep it.
+  compatible_runtimes      = ["python3.7"]                     # Optional argument, but keep it.
   description              = "Python Library."                 # Optional argument, but keep it.
   filename                 = local.layer_zip                   # Optional argument, conflicts with s3_bucket, s3_key and s3_object_version.
   license_info             = "Apache License 2.0"              # Optional argument, but keep it.
