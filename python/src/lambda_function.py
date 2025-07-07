d9543f6eaae59b673ece939f323650d6d68697fb
Balaji Pothula <balan.pothula@gmail.com>
2025-07-07 12:38:23 +0530

    Testing Lambda.

--- python/src/lambda_function.py
+++ python/src/lambda_function.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from mangum import Mangum
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 app = FastAPI()
 
 @app.get("/")
 def root():
+    logger.info("GET / hit")
     return {"message": "Hello from FastAPI on Lambda!"}
 
 # Required for AWS Lambda to trigger FastAPI
