24032b44be5399e0f6088d6d535ad1c886f1ce03
Balaji Pothula <balan.pothula@gmail.com>
2025-07-08 18:23:30 +0530

    Testing Lambda.

--- python/src/lambda_function.py
+++ python/src/lambda_function.py
@@ -129,6 +129,9 @@ def getEngine(postgresql: dict):
     })
     logger.error(errorMessage)    
 
+
+region     = os.environ["region"]
+secret     = os.environ["secret"]
 """
 postgresql = getCredentials(region = region, secret = secret)
 url        = postgresql["dialect"] + "+" + postgresql["driver"] + "://" + postgresql["username"] + ":" + postgresql["password"] + "@" + postgresql["host"] + ":" + str(postgresql["port"]) + "/" + postgresql["database"]
