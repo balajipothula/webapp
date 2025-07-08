64545c38ecdc5c2607f09d9bf367edbd202d0c81
Balaji Pothula <balan.pothula@gmail.com>
2025-07-08 17:41:06 +0530

    Testing Lambda

--- python/src/lambda_function.py
+++ python/src/lambda_function.py
@@ -131,12 +131,12 @@ def getEngine(postgresql: dict):
 
 region     = os.environ["region"]
 secret     = os.environ["secret"]
+"""
 postgresql = getCredentials(region = region, secret = secret)
 url        = postgresql["dialect"] + "+" + postgresql["driver"] + "://" + postgresql["username"] + ":" + postgresql["password"] + "@" + postgresql["host"] + ":" + str(postgresql["port"]) + "/" + postgresql["database"]
 database   = databases.Database(url)
 engine     = getEngine(postgresql)
 metadata   = sqlalchemy.MetaData()
-"""
 metadata.create_all(engine)
 
 song       = sqlalchemy.Table(
