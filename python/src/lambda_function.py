87966f402b56cd82f428d0cf719912458e11250a
Balaji Pothula <balan.pothula@gmail.com>
2025-07-17 13:27:17 +0530

    testing lambda

--- python/src/lambda_function.py
+++ python/src/lambda_function.py
@@ -1,5 +1,50 @@
 import boto3
+import json
+import logging
+import os
+import psycopg
+import sys
+import traceback
+import base64
+import gzip
+import os
+import datetime
+
 from botocore.exceptions import ClientError
+
+from json.decoder import JSONDecodeError
+
+from sqlalchemy import create_engine
+from sqlalchemy import engine
+from sqlalchemy import text
+
+from sqlalchemy.engine.interfaces import Dialect
+from sqlalchemy.engine.url import URL
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import session
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import insert
+
+from fastapi import FastAPI
+from fastapi import Request
+
+from mangum  import Mangum
+
+from pydantic import BaseModel
+
+from typing import List
+
+import databases
+import sqlalchemy
+
+logging.getLogger("boto3").setLevel(logging.ERROR)
+logging.getLogger("botocore").setLevel(logging.ERROR)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
 
 def lambda_handler(event, context):
     secret_name = "webapp-db-credentials-1"
