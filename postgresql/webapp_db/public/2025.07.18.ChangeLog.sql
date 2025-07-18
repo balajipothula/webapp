11bd648a1c7b36568ef55e78210ed22d0b482e07
Balaji Pothula <balan.pothula@gmail.com>
2025-07-18 17:58:41 +0530

    Run Liquibase GitHub Action

--- postgresql/webapp_db/public/2025.07.18.ChangeLog.sql
+++ postgresql/webapp_db/public/2025.07.18.ChangeLog.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset BalajiPothula:2025-07-15T12:10:10Z
+-- changeset BalajiPothula:2025-07-18T12:30:10Z
 
 -- create Song table.
 CREATE TABLE IF NOT EXISTS webapp_db.public."Song"(
