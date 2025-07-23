5b4ad8ba01fc8356424df429b789fdda7c1ea92e
Balaji Pothula <balan.pothula@gmail.com>
2025-07-23 16:11:07 +0530

    Restore point 2025-07-23-16-10

---
+++ shell/cURL.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Author      : Balaji Pothula <balan.pothula@gmail.com>,
+# Date        : Wednesday, 23 July 2025,
+# Description : cURL commands.
+
+# Insert song record into webapp_db.public."Song" table.
+# Compatible with Power Shell and Unix shell.
+curl -X PUT https://fgsb6aofnk.execute-api.eu-central-1.amazonaws.com/insert/song -H "Content-Type: application/json" -d '{ "artist": "Linkin Park", "title": "In the End", "difficulty": 3.7, "level": 5, "released": "2000-10-24" }'
+
+# 
+curl -X GET https://fgsb6aofnk.execute-api.eu-central-1.amazonaws.com/select/songs -H "Accept: application/json"
