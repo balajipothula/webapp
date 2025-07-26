8bc093a401d1258a178a443d2453d67b9f8f38ef
Balaji Pothula <balan.pothula@gmail.com>
2025-07-26 15:45:09 +0530

    testing.

--- terraform/aws/ecrpublic/repository/main.tf
+++ terraform/aws/ecrpublic/repository/main.tf
@@ -7,8 +7,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 5.75.1"
     }
   }
+  required_version = ">= 1.0.5"
 }
 
 resource "aws_ecrpublic_repository" "generic" {
