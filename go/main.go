b0fa941e3cbeef94957570b66987eec31264dded
Balaji Pothula <balan.pothula@gmail.com>
2025-08-06 20:14:35 +0530

    testing.

---
+++ go/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	webapp := fiber.New(fiber.Config{
+		DisableStartupMessage: true,
+	})
+
+	webapp.Get("/", func(context *fiber.Ctx) error {
+		return context.SendString("Hello, world 🚀")
+	})
+
+	webapp.Listen(":80")
+}
