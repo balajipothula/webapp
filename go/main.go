992d8ebf59f4139a2761fe14f5fb641651fc729c
Balaji Pothula <balan.pothula@gmail.com>
2025-08-07 12:58:14 +0530

    testing.

--- go/main.go
+++ go/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
+  "github.com/gofiber/fiber/v2"
 )
 
 func main() {
 
-	webapp := fiber.New(fiber.Config{
-		DisableStartupMessage: true,
-	})
+  webapp := fiber.New(fiber.Config{
+    DisableStartupMessage: true,
+  })
 
-	webapp.Get("/", func(context *fiber.Ctx) error {
-		return context.SendString("Hello, world 🚀")
-	})
+  webapp.Get("/", func(context *fiber.Ctx) error {
+    return context.SendString("Hello, world 🚀")
+  })
 
-	webapp.Listen(":80")
+  webapp.Listen(":80")
+
 }
